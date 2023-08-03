a0e4000f9d197e48743059d79643e99fb7e42934
kkomyoung <kya754@gmail.com>
2023-08-03 22:23:54 +0900

    home 모션 추가

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,6 +1,7 @@
 /** @jsxImportSource @emotion/react */
 import React from "react";
 import { css } from "@emotion/react";
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
@@ -8,12 +9,16 @@ export default function Home() {
       css={css`
         background: #212121;
         min-height: 100vh;
+        position: relative;
+        overflow: hidden;
       `}
     >
       <div
         css={css`
+          position: relative;
+          z-index: 2;
           margin: 0 auto;
-          max-width: 1026px;
+          max-width: 1236px;
           padding: 0 40px;
           min-height: 100vh;
           display: flex;
@@ -22,7 +27,7 @@ export default function Home() {
       >
         <h1
           css={css`
-            font-size: 80px;
+            font-size: 90px;
             font-weight: 700;
             color: #fff;
             & span {
@@ -34,6 +39,54 @@ export default function Home() {
           <span>프론트엔드 개발자</span>
           <span>김영애입니다</span>
         </h1>
+      </div>
+      <div
+        css={css`
+          position: absolute;
+          bottom: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          transform: rotate(-22deg);
+        `}
+      >
+        <motion.div
+          css={css`
+            position: absolute;
+            bottom: 0;
+            width: 90vw;
+            height: 23vw;
+            border-radius: 400px;
+            background: #8b5cf6;
+          `}
+          initial={{ left: "-92vw" }}
+          animate={{ left: "-30vw" }}
+          transition={{
+            delay: 0.7,
+            duration: 0.7,
+            type: "spring",
+            stiffness: 80,
+          }}
+        />
+        <motion.div
+          css={css`
+            position: absolute;
+            left: 65%;
+            bottom: 0;
+            width: 23vw;
+            height: 23vw;
+            border-radius: 50%;
+            background: #8b5cf6;
+          `}
+          initial={{ opacity: 0, left: "50%" }}
+          animate={{ opacity: 1, left: "65%" }}
+          transition={{
+            delay: 0.9,
+            duration: 0.2,
+            type: "spring",
+            stiffness: 100,
+          }}
+        />
       </div>
     </section>
   );
