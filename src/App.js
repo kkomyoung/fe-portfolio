0db274f39a91b6b93e7de0e55a177c7d291711ba
kkomyoung <kya754@gmail.com>
2023-08-23 19:17:22 +0900

    feat: 페이지 이동시 scrollToTop 기능 추가

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import Resume from "./pages/ResumePage";
 import Main from "./pages/MainPage";
+import DailyDevPage from "./pages/projects/DailyDevPage";
+import ScrollToTop from "./utils/ScrollToTop";
 
 function App() {
   return (
     <main className="App">
       <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <ScrollToTop />
         <AnimatePresence>
           <Routes>
             <Route path="/" element={<Main />} />
+            <Route path="/daily-dev" element={<DailyDevPage />} />
             <Route path="/resume" element={<Resume />} />
           </Routes>
         </AnimatePresence>
